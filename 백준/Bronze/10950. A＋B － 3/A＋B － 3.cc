a92eea418da6fdead17132d10d4d633f38093731
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2026-03-12 16:03:40 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int Count = 0;
+	cin >> Count;
+	for (int i = 0; i < Count; ++i)
+	{
+		int A, B;
+		cin >> A >> B;
+		cout << A + B << '\n';
+	}
+
+	return 0;
+}
