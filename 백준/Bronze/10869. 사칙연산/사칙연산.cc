f3d8c8b5c93b191a1edcc4bd5b4a4056b984be81
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2026-03-12 15:08:40 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int A, B;
+	cin >> A >> B;
+
+	cout << static_cast<long>(A) + B << '\n';
+	cout << static_cast<long>(A) - B << '\n';
+	cout << static_cast<long>(A) * B << '\n';
+	cout << static_cast<long>(A) / B << '\n';
+	cout << static_cast<long>(A) % B << '\n';
+
+	return 0;
+}
