cff97d6668ac520ed89baf47282fdc5427115cdb
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2026-03-16 16:23:19 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2208 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int Hashing(const int& Length, const string& Str)
+{
+	int Value{};
+	for (int i = 0; i < Length; ++i)
+	{
+		Value += pow(31, i) * (Str[i] - 'a' + 1);
+	}
+
+	return Value;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int Length{};
+	cin >> Length;
+	string Str{};
+	cin >> Str;
+	int Value{};
+	Value = Hashing(Length, Str);
+	cout << Value << '\n';
+}
