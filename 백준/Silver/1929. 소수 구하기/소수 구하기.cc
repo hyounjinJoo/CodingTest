df0e40127ab30a69025911081822329e8fbe4b79
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2026-03-21 03:46:12 +0900

    [Silver III] Title: 소수 구하기, Time: 8 ms, Memory: 2260 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -7,27 +8,25 @@ int main()
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	int MAXIDX = 1000001;
-	vector<int> V(MAXIDX);
-	for (int i = 2; i < MAXIDX; ++i)
-	{
-		V[i] = i;
-	}
+	int M, N;
+	cin >> M >> N;
 
-	int Limit = MAXIDX * 0.5;
-	for (int i = 2; i <= Limit; ++i)
+	vector<bool> IsPrime(N + 1, true);
+	IsPrime[0] = IsPrime[1] = false;
+
+	for (int i = 2; i * i <= N; ++i)
 	{
-		for (int j = 2; i <= Limit; ++j)
+		if (IsPrime[i])
 		{
-			if (i * j >= MAXIDX) break;
-			V[i * j] = 0;
+			for (int j = i * i; j <= N; j += i)
+			{
+				IsPrime[j] = false;
+			}
 		}
 	}
 
-	int M{}, N{};
-	cin >> M >> N;
 	for (int i = M; i <= N; ++i)
 	{
-		if (V[i] != 0) cout << V[i] << '\n';
+		if (IsPrime[i]) cout << i << '\n';
 	}
 }
