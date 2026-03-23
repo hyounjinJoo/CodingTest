c6c0f62fe6cde2dd13b4baf4fd73964d873a9eac
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2026-03-23 14:26:44 +0900

    [Gold V] Title: 토마토, Time: 80 ms, Memory: 10256 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,62 +1,71 @@
-#include <queue>
-#include <utility>
+// 7576 복습
 #include <iostream>
-using namespace std;
+#include <utility>
+#include <vector>
+#include <queue>
 #define X first
 #define Y second
-int Board[1002][1002];
-int Dist[1002][1002];
-int N, M;
-int Dx[4] = { 1, 0, -1, 0 };
-int Dy[4] = { 0, 1, 0, -1 };
+using namespace std;
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, -1, 0, 1 };
+
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
+	int M{}, N{};
 	cin >> M >> N;
+	vector<vector<int>> Box(N, vector<int>(M));
 	queue<pair<int, int>> Q;
+
+	bool IsAlready = true;
 	for (int i = 0; i < N; ++i)
 	{
 		for (int j = 0; j < M; ++j)
 		{
-			cin >> Board[i][j];
-			if (Board[i][j] == 1)
-				Q.push({ i, j });
-			if (Board[i][j] == 0)
-				Dist[i][j] = -1;
+			cin >> Box[i][j];
+			if (Box[i][j] == 0) IsAlready = false;
+			if (Box[i][j] == 1) Q.push({ i, j });
 		}
 	}
+	if (IsAlready)
+	{
+		cout << 0;
+		return 0;
+	}
 
+	int Day = -1;
 	while (!Q.empty())
 	{
-		auto Cur = Q.front(); Q.pop();
-		for (int Dir = 0; Dir < 4; ++Dir)
+		++Day;
+		int PerDayCan = Q.size();
+		while (PerDayCan--)
 		{
-			int Nx = Cur.X + Dx[Dir];
-			int Ny = Cur.Y + Dy[Dir];
-
-			if (Nx < 0 || Nx >= N || Ny < 0 || Ny >= M) continue;
-			if (Dist[Nx][Ny] >= 0) continue;
-			Dist[Nx][Ny] = Dist[Cur.X][Cur.Y] + 1;
-			Q.push({ Nx, Ny });
+			auto Cur = Q.front(); Q.pop();
+			for (int i = 0; i < 4; ++i)
+			{
+				int nx = Cur.Y + dx[i];
+				int ny = Cur.X + dy[i];
+				if (nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
+				if (Box[ny][nx] != 0) continue;
+				Box[ny][nx] = 1;
+				Q.push({ ny, nx });
+			}
 		}
 	}
-	int Ans = 0;
+
 	for (int i = 0; i < N; ++i)
 	{
 		for (int j = 0; j < M; ++j)
 		{
-			if (Dist[i][j] == -1)
+			if (Box[i][j] == 0)
 			{
 				cout << -1;
 				return 0;
 			}
-			Ans = max(Ans, Dist[i][j]);
 		}
 	}
 
-	cout << Ans;
-
-	return 0;
+	cout << Day;
 }
